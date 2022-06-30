f6a704079dc2ce62a7242e4eff1b1e96cef6d33d
Mohamed Khaled <1leomedo1@gmail.com>
2022-06-30 17:10:54 +0200

    categories

--- lib/screens/categories/categories_screen.dart
+++ lib/screens/categories/categories_screen.dart
@@ -1,74 +1,87 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:se_commerce/layout/social_app/cubit/cubit.dart';
-import 'package:se_commerce/layout/social_app/cubit/states.dart';
-import 'package:se_commerce/styles/themes.dart';
-
-
+import 'cubit/cubit.dart';
+import 'cubit/states.dart';
 
 class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    var cubit = SocialCubit.get(context);
+    List<String> categoriesImg = [
+      'assets/images/mobiles.jpg',
+      'assets/images/electronics.jpg',
+      'assets/images/accessories.jpg',
+      'assets/images/devices.jpg',
+    ];
+    List<String> categoriesTitles = [
+      'Mobiles',
+      'Electronics',
+      'Accessories',
+      'Electrical Devices',
+    ];
 
-    return BlocConsumer<SocialCubit, SocialStates>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        return Scaffold(
-          body: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: GridView.builder(
-                scrollDirection: Axis.vertical,
-                physics: BouncingScrollPhysics(),
-                padding: EdgeInsets.all(10),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  childAspectRatio: 2.2,
-                  mainAxisSpacing: 0.0,
-                  crossAxisSpacing: 0.0,
-                ),
-                itemCount: cubit.categoriesImg.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 150,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4.0),
-                            image: DecorationImage(
-                                image: AssetImage(cubit.categoriesImg[index]),
-                                fit: BoxFit.cover,
-                                colorFilter: ColorFilter.mode(
-                                    Colors.black45, BlendMode.hardLight)),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: Text.rich(
-                            TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: cubit.categoriesTitles[index],
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20.0),
-                                ),
-                              ],
+    return BlocProvider(
+      create: (context) => CategoryCubit(),
+      child: BlocConsumer<CategoryCubit, CategoryStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          CategoryCubit cubit = CategoryCubit.get(context);
+          return Scaffold(
+            body: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: GridView.builder(
+                  scrollDirection: Axis.vertical,
+                  physics: const BouncingScrollPhysics(),
+                  padding: const EdgeInsets.all(10),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    childAspectRatio: 2.2,
+                    mainAxisSpacing: 0.0,
+                    crossAxisSpacing: 0.0,
+                  ),
+                  itemCount: categoriesImg.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4.0),
+                              image: DecorationImage(
+                                  image: AssetImage(categoriesImg[index]),
+                                  fit: BoxFit.cover,
+                                  colorFilter: const ColorFilter.mode(
+                                      Colors.black45, BlendMode.hardLight)),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
-          ),
-        );
-      },
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: categoriesTitles[index],
+                                    style: const TextStyle(
+                                        color: Colors.white, fontSize: 20.0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/screens/categories/cubit/states.dart
+++ lib/screens/categories/cubit/states.dart
@@ -1,25 +1,4 @@
-abstract class SocialStates {}
-class SocialInitialState extends SocialStates{}
+abstract class CategoryStates {}
 
-class SocialChangeBottomNavState extends SocialStates{}
+class CategoryInitialState extends CategoryStates{}
 
-class SocialProductLoadingState extends SocialStates{}
-class SocialProductSuccessState extends SocialStates{}
-class SocialProductErrorState extends SocialStates{
-  final String error;
-  SocialProductErrorState(this.error);
-}
-class SocialAddProductSuccessState extends SocialStates{}
-class SocialRemoveProductSuccessState extends SocialStates{}
-
-class SocialChangeImageState extends SocialStates{}
-
-class SocialIsMoreChange extends SocialStates{}
-
-class SocialIsFavChange extends SocialStates{}
-
-class SocialCheckFav extends SocialStates{}
-
-class SocialDataShared extends SocialStates{}
-
-class SocialCartData extends SocialStates{}
